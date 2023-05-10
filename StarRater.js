661efad921c44ea3eaaa1ee273314445ff4b7264
Dayana Mariano <dayanaraquelle@gmail.com>
2023-05-10 16:39:37 -0300

    create web component

--- StarRater.js
+++ StarRater.js
@@ -9,17 +9,39 @@ class StarRater extends HTMLElement {
     const shadow = this.attachShadow({mode: 'open'})
 
     shadow.appendChild(this.styles())
+
+    const rater = this.createRater()
+    const stars = this.createStars()
+
+    stars.forEach( star => rater.appendChild(star))
+
+    shadow.appendChild(rater)
+  }
+
+  createRater() {
+    const rater = document.createElement('div')
+    rater.classList.add('star-rater')
+    return rater
+  }
+
+  createStars() {
+    const createStar = (_, id) => {
+      const star = document.createElement('span')
+      star.classList.add('star')
+      star.setAttribute('data-value', Number(id) + 1)
+      star.innerHTML = '&#9733;'
+      return star
+    }
+
+    return Array.from({ length: 5 }, createStar)
   }
 
   styles() {
     const style = document.createElement('style')
     style.textContent = `
-      .star-rater {
-        background-color: #f00;
-      }
     `
     return style
   }
 }
 
-customElements.define('star-rater', StarRater)
+customElements.define('star-rater', StarRater) 
